613b60c1c61b8cc69a9ad33341431f011c430779
Piotr Kowalski <piecioshka@gmail.com>
2015-04-24 12:29:37 +0200

    Reformat.

--- js/todolist.js
+++ js/todolist.js
@@ -1,123 +1,123 @@
-(function(){
-//
-window.App = {
-	Models: {},
-	Collections: {},
-	Views: {}
-}; 
+(function () {
+    window.App = {
+        Models: {},
+        Collections: {},
+        Views: {}
+    };
 
-window.template = function(id){
-	return _.template( $('#' + id).html() );
-};
+    window.template = function (id) {
+        return _.template($('#' + id).html());
+    };
 
-App.Models.Task = Backbone.Model.extend({
-/
-		validate : function(attrs){   
-		if( !attrs.title || !$.trim(attrs.title)){
-			throw new ReferenceError('Put a valid title!');
-		} 
-	}
+    App.Models.Task = Backbone.Model.extend({
+        validate: function (attrs) {
+            if (!attrs.title || !$.trim(attrs.title)) {
+                throw new ReferenceError('Put a valid title!');
+            }
+        }
+    });
 
-});
+    App.Collections.Tasks = Backbone.Collection.extend({
+        type: App.Models.Task
+    });
 
-App.Collections.Tasks = Backbone.Collection.extend({
-	type : App.Models.Task
-});
+    App.Views.Tasks = Backbone.View.extend({
+        tagName: 'ul',
 
-App.Views.Tasks = Backbone.View.extend({
-	tagName : 'ul',
-	initialize : function(){
-		this.collection.on('add', this.addOne, this);
-	},
-	render : function(){
-		this.collection.each(this.addOne, this);
-		return this;
-	},
+        initialize: function () {
+            this.collection.on('add', this.addOne, this);
+        },
 
-	addOne : function(task){
-		//creating new child view
-		var taskView = new App.Views.Task({model : task});
-	/*	taskView.render();*/
-		//append to the root el 
-		this.$el.append(taskView.render().el);
-	}
+        render: function () {
+            this.collection.each(this.addOne, this);
+            return this;
+        },
 
-});
+        addOne: function (task) {
+            //creating new child view
+            var taskView = new App.Views.Task({ model: task });
+            /*	taskView.render();*/
+            //append to the root el
+            this.$el.append(taskView.render().el);
+        }
+    });
 
-App.Views.Task = Backbone.View.extend({
-	tagName : 'li',
-	initialize: function(){
-		/*_.bindAll(this, 'editTask', 'render');*/ // bind kontekstu metod
-		
-		this.model.on('change', this.render, this);//kiedy-metoda-konteks
-		this.model.on('destroy', this.remove, this);
-	},
+    App.Views.Task = Backbone.View.extend({
+        tagName: 'li',
 
-	template : template('taskTemplate'), 
+        initialize: function () {
+            /*_.bindAll(this, 'editTask', 'render');*/ // bind kontekstu metod
 
-	events :{
-		'click .edit' : 'editTask' ,
-		'click .delete' : 'destroy'
-	},
-	destroy : function(){
-		this.model.destroy();
-		console.log(tasksCollection);
-	},
-	remove : function(){
-		this.$el.remove();
-	},
-	editTask : function(){
-		var newTaskTitle = prompt('What?', this.model.get('title'));
-		if(!newTaskTitle || !$.trim(newTaskTitle)) return;
+            this.model.on('change', this.render, this); // kiedy-metoda-kontekst
+            this.model.on('destroy', this.remove, this);
+        },
 
-		this.model.set('title', newTaskTitle, {validate:true});
+        template: template('taskTemplate'),
 
-	},
+        events: {
+            'click .edit': 'editTask',
+            'click .delete': 'destroy'
+        },
 
-	render : function(){
-		var template = this.template(this.model.toJSON());
-		this.$el.html(template);
-		return this;
-	} 
+        destroy: function () {
+            this.model.destroy();
+            console.log(tasksCollection);
+        },
 
-});
+        remove: function () {
+            this.$el.remove();
+        },
 
-App.Views.AddTask = Backbone.View.extend({
-	el : '#addTask',
+        editTask: function () {
+            var newTaskTitle = prompt('What?', this.model.get('title'));
+            if (!newTaskTitle || !$.trim(newTaskTitle)) return;
 
-	events : {
-		'submit' : 'submit'
-	},
+            this.model.set('title', newTaskTitle, { validate: true });
 
-	submit : function(e){
-		e.preventDefault();
-		var newTaskTitle = $(e.currentTarget).find('input[type=text]').val();
-		//{validate:true}!
-		var task = new App.Models.Task({title : newTaskTitle}, {validate:true});
-		this.collection.add(task);
-	}
+        },
 
-});
+        render: function () {
+            var template = this.template(this.model.toJSON());
+            this.$el.html(template);
+            return this;
+        }
 
+    });
 
-window.tasksCollection = new App.Collections.Tasks([
-	{
-		title : 'Go to the store'
+    App.Views.AddTask = Backbone.View.extend({
+        el: '#addTask',
 
-	},
-	{
-		title : 'Go to school'
-	},
-	{
-		title : 'Eas dinner'
-	}
-]);
+        events: {
+            'submit': 'submit'
+        },
 
-var tasksView = new App.Views.Tasks({ collection: tasksCollection});
-var addTaskView = new App.Views.AddTask({collection:tasksCollection});
+        submit: function (e) {
+            e.preventDefault();
+            var newTaskTitle = $(e.currentTarget).find('input[type=text]').val();
+            //{validate:true}!
+            var task = new App.Models.Task({ title: newTaskTitle }, { validate: true });
+            this.collection.add(task);
+        }
 
-$('.tasks').html(tasksView.render().el);
+    });
 
+    window.tasksCollection = new App.Collections.Tasks([
+        {
+            title: 'Go to the store'
 
+        },
+        {
+            title: 'Go to school'
+        },
+        {
+            title: 'Eas dinner'
+        }
+    ]);
 
-})();
+    var tasksView = new App.Views.Tasks({ collection: tasksCollection });
+
+    // Nie używane...
+    var addTaskView = new App.Views.AddTask({ collection: tasksCollection });
+
+    $('.tasks').html(tasksView.render().el);
+}());
